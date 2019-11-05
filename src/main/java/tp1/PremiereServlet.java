c77c76d78d5f40e29c8b9b4ef8716e44010ce0f7
orchaland <orchaland@gmail.com>
2019-11-05 16:03:51 +0100

    premiere servlet

---
+++ src/main/java/tp1/PremiereServlet.java
@@ -0,0 +1,17 @@
+package tp1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PremiereServlet extends HttpServlet{
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("nom");
+		System.out.println("name = " + name);
+	}
+	
+}
